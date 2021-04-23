01849931d51be823fc806f26ed0db6249b34e0fe
Breno Martins <brenomsimao123@outlook.com>
2021-04-23 13:52:15 -0300

    Add public page and support for WebSocket

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,43 @@
 //Dependences
-import express from "express"
-import bodyParser from "body-parser"
+import express, { response } from "express"
+
+//Importing path
+import path from "path"
+
+//importing websocket Dependences
+import {createServer} from "http"; 
+import {Server, Socket} from "socket.io";
+
 //DB dependences
 import "./database";
+//Importing Routes
 import {routes} from "./routes"
 //Static Config
 const port = 3333
 
 //Instacing Express
 const app = express()
+
+app.use(express.static(path.join(__dirname,"..", "public")))
+app.set("views", path.join(__dirname,"..", "public"))
+app.engine("html", require("ejs").renderFile)
+app.set("view engine", "html")
+
+app.get("/pages/client", (req, res)=>{
+    return res.render("html/client.html")
+})
+
+const http = createServer(app)//Creating Protocol HTTP
+const io = new Server(http)//Creating Protocol WebSocket (WS)
+
+io.on("connection", (socket:Socket)=>{
+    console.log("Se conectou", socket.id)
+})
+
 app.use(express.json())
 
 //routes
 app.use(routes)
 
 //Starting 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+http.listen(port, () => console.log(`Server running on port ${port}`))
